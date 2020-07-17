6013bd25f51606166d52f08ec6953acc900535ef
Krishnath <shadyanuj24@gmail.com>
2020-07-17 22:04:50 +0530

    added styles for individual pages

--- NodeWorkspace/codeial/index.js
+++ NodeWorkspace/codeial/index.js
@@ -4,6 +4,9 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 app.use(express.static('./assets'));
 
 app.set('view engine','ejs');
