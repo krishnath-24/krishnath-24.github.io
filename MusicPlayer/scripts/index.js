c5a1d0dd1c4cddc9a43eb25d95b9874d1bb37b1f
Krishnath <shadyanuj24@gmail.com>
2020-07-20 15:06:47 +0530

    restructured the app folders and created the second page

---
+++ MusicPlayer/scripts/index.js
@@ -0,0 +1,25 @@
+    
+(function(){
+    $(document).scroll(function() {
+        var y = $(this).scrollTop();
+        if (y > 100) {
+            $('.media-controls-container').fadeIn();
+            $('.media-controls-container').css({
+                "display": "flex"
+            });
+        } 
+    });
+
+
+    function show(shown, hidden) {
+
+      document.getElementById(shown).style.display='block';
+      document.getElementById(hidden).style.display='none';
+      console.log(shown);   
+      if(shown == "page2") {
+        document.getElementById(shown).style.display='flex';
+      }
+      window.scrollTo(0, 0);
+      return false;
+    }
+})();
