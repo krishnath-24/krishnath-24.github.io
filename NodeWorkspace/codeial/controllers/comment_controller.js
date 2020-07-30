0344cf7a1dc739caba9aee340d71eb47afb2eca1
Krishnath <shadyanuj24@gmail.com>
2020-07-30 13:52:37 +0530

    display comments on each post with the comment author name

--- NodeWorkspace/codeial/controllers/comment_controller.js
+++ NodeWorkspace/codeial/controllers/comment_controller.js
@@ -3,30 +3,25 @@ const Post = require('../models/post');
 
 module.exports.create = (req, res)=>{
 
-    let comment = {
-        content : req.body.content,
-        user : req.user.id,
-        post : req.params.id
-    }
+    let postId = req.params.id;
 
-    
+    Post.findById(postId, (error, post)=>{
 
-    Comment.create(comment,(error, comment)=>{
+        if(post) {
 
-        if(error) {
-            console.log(error);
-            return res.redirect('/');
+            Comment.create({
+                content : req.body.content,
+                user : req.user.id,
+                post : postId
+            },(error, comment)=>{
+
+                if(comment) {
+                    post.comment.push(comment.id);
+                    post.save();
+                }
+            });
         }
 
-        Post.findById(req.params.id,(error,post)=>{
-            let newPost = post;
-            newPost.comment.push(comment.id);
-            Post.findByIdAndUpdate(req.params.id,newPost,(error,post)=>{
-                console.log(post);
-            });
-        });
-
-        console.log(comment);
         return res.redirect('/');
     });
 }
--- NodeWorkspace/codeial/controllers/home_controller.js
+++ NodeWorkspace/codeial/controllers/home_controller.js
@@ -3,7 +3,14 @@ const Post = require('../models/post');
 
 module.exports.home = function(req,res) {
     
-    Post.find({}).populate('user').exec((error,posts)=>{
+    Post.find({}).populate('user')
+    .populate({
+        path : 'comment',
+        populate : {
+            path : 'user'
+        }
+    })
+    .exec((error,posts)=>{
 
         return res.render('home',{
             title : 'home',
