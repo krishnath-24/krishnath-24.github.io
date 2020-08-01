d592a88bcd6dc89a33ed673ca53feea14fe70efa
Krishnath <shadyanuj24@gmail.com>
2020-08-01 13:33:54 +0530

    styled the skill progress and the project

--- NodeWorkspace/codeial/controllers/post_controller.js
+++ NodeWorkspace/codeial/controllers/post_controller.js
@@ -1,16 +1,36 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.createPost = (req, res)=> {
     
     Post.create({
         content : req.body.content,
         user : req.user.id
+
     },(error,post)=>{
         if(error) {
             console.log(error);
             return res.redirect('back');
         }
-        console.log(post);
         res.redirect('/');
+    });
+}
+
+module.exports.destroy = (req, res) => {
+
+    Post.findById(req.params.id,(error,post)=>{
+
+        if(post) {
+            if(post.user == req.user.id) {
+                post.remove();
+                
+                Comment.deleteMany({post : req.params.id},(error)=>{
+                    return res.redirect('back');
+                });
+            }
+        }
+        else {
+            return res.redirect('back');
+        }
     });
 }
--- NodeWorkspace/codeial/routes/posts.js
+++ NodeWorkspace/codeial/routes/posts.js
@@ -2,10 +2,9 @@ const passport = require('passport');
 const express = require('express');
 const router = express.Router();
 
-
 const postController = require('../controllers/post_controller');
 
 router.post('/create-post',passport.checkAuthenticated,postController.createPost);
 
-
+router.get('/destroy/:id',passport.checkAuthenticated,postController.destroy);
 module.exports = router;
