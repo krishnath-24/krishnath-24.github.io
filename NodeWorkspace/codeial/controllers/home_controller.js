178684ecbcfadc4ee25d24787d010016be01a310
Krishnath <shadyanuj24@gmail.com>
2020-08-09 13:38:13 +0530

    changed home controller actions to use async await

--- NodeWorkspace/codeial/controllers/home_controller.js
+++ NodeWorkspace/codeial/controllers/home_controller.js
@@ -1,24 +1,33 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res) {
+// define the home action
+module.exports.home = async function(req,res) {
     
-    Post.find({}).populate('user')
-    .populate({
-        path : 'comment',
-        populate : {
-            path : 'user'
-        }
-    })
-    .exec((error,posts)=>{
-
-        User.find({},(error,users)=>{
-            return res.render('home',{
-                title : 'home',
-                posts : posts,
-                all_users : users
-            });
+    try {
+        // find all the posts
+        let posts = await Post.find({}).populate('user');
+        post.populate({
+            path : 'comment',
+            populate : {
+                path : 'user'
+            }
         });
-    });
-    
+
+        // find all the users
+        let users = await User.find({});
+
+        // render the home page
+        return res.render('home',{
+            title : 'home',
+            posts : posts,
+            all_users : users
+        });
+
+        
+    } catch (error) { // if an error occurs
+        
+        console.log(error);
+        return res.redirect('back'); // redirect back
+    }
 }
